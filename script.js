4e36fcb41471c879fb89d39b5b3fdf441a758f2b
PranayTyagi1 <>
2021-05-05 13:49:33 +0000

    added let and added css

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.querySelector('button').addEventListener('click', function () {
-  guess = document.querySelector('input').value
+  let guess = document.querySelector('input').value
   guess = parseInt(guess)
-  randomNumber = Math.floor(Math.random() * 6) + 1
+  let randomNumber = Math.floor(Math.random() * 6) + 1
   if (randomNumber == guess) {
     alert('correct')
   } else {
