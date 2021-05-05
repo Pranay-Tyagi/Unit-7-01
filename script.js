e338c7b91e16d3c2083deff2a204b476212598d7
PranayTyagi1 <>
2021-05-05 13:54:37 +0000

    changed let to const

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.querySelector('button').addEventListener('click', function () {
   let guess = document.querySelector('input').value
   guess = parseInt(guess)
-  let randomNumber = Math.floor(Math.random() * 6) + 1
-  if (randomNumber == guess) {
+  const randomNumber = Math.floor(Math.random() * 6) + 1
+  if (randomNumber === guess) {
     alert('correct')
   } else {
     alert('wrong')
